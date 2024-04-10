4ce5f8eeec02dbbb1863ea7ef432d98e541473fb
TigerDemon <128728704+tigerdemon5@users.noreply.github.com>
2024-04-10 17:46:02 +0900

    Update Member.java

--- src/main/java/com/likelion12th/shop/entity/Member.java
+++ src/main/java/com/likelion12th/shop/entity/Member.java
@@ -1,6 +1,50 @@
 package com.likelion12th.shop.entity;
 
+import com.likelion12th.shop.constant.Role;
+import com.likelion12th.shop.dto.MemberFormDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="member")
+@Getter
+@Setter
+@ToString
 public class Member {
 
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
 
+    private String name;
+
+    @Column(unique=true)
+    private String email;
+    private String password;
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private LocalDateTime createdBy;
+    private LocalDateTime modifiedBy;
+
+
+
+    public static Member createMember(MemberFormDto memberFormDto){
+        Member member= new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setPassword(memberFormDto.getPassword());
+        member.setAddress(memberFormDto.getAddress());
+
+
+        return member;
+    }
 }
+
