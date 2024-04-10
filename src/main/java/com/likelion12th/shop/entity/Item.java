986ab611bbe906190535b43dbb069f93ccc05d15
Seoheeuhm <seohee030128@naver.com>
2024-04-10 10:45:32 +0900

    first commit.

--- src/main/java/com/likelion12th/shop/entity/Item.java
+++ src/main/java/com/likelion12th/shop/entity/Item.java
@@ -1,4 +1,29 @@
 package com.likelion12th.shop.entity;
 
+import com.likelion12th.shop.constant.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
 public class Item {
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer stock;
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private com.likelion12th.shop.constant.itemSellStatus itemSellStatus;
+
+    private LocalDateTime createdBy;
+    private LocalDateTime modifiedBy;
 }
--- src/main/java/com/likelion12th/shop/entity/Payment.java
+++ src/main/java/com/likelion12th/shop/entity/Payment.java
@@ -3,31 +3,30 @@ package com.likelion12th.shop.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table
+@Table(name="payment")
 @Getter
 @Setter
-public class OrderItem {
+@ToString
+public class Payment {
     @Id
-    @Column(name = "orderitem_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //Order
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
 
-    //Item
-    @ManyToOne
-    @JoinColumn(name = "item_id")
-    private Item item;
+    private Integer amount;
+    private Integer price;
+    private String paymentMethod;
+    private String customerId;
 
-    private Integer orderPrice;
-    private Integer count;
     private LocalDateTime createdBy;
     private LocalDateTime modifiedBy;
 }
+
