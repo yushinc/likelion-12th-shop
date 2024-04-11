d531fc50eb27fb4955b6b32fc2c0c3eb09e5a86b
yerim <97941141+yerim123456@users.noreply.github.com>
2024-04-11 12:36:42 +0900

    feat: Add order entity & order status enum

--- src/main/java/com/likelion12th/shop/entity/Order.java
+++ src/main/java/com/likelion12th/shop/entity/Order.java
@@ -1,4 +1,33 @@
 package com.likelion12th.shop.entity;
 
+
+import com.likelion12th.shop.constant.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
 public class Order {
+
+    @Id
+    @Column(name = "order_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime OrderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private LocalDateTime createdBy;
+
+    private LocalDateTime modifiedBy;
+
 }
