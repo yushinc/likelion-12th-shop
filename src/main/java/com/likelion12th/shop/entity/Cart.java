ac3212e539dd5a2084984fe1bed28343cc153d42
yerim <97941141+yerim123456@users.noreply.github.com>
2024-04-11 12:34:49 +0900

    feat: Add cart entity

--- src/main/java/com/likelion12th/shop/entity/Cart.java
+++ src/main/java/com/likelion12th/shop/entity/Cart.java
@@ -1,4 +1,24 @@
 package com.likelion12th.shop.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cart")
+@Getter
 public class Cart {
+
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime createdBy;
+    private LocalDateTime modifiedBy;
 }
