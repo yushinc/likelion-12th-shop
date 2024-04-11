e01e0a12b3793de4a5c14ad6d2354d1c72ea600d
yerim <97941141+yerim123456@users.noreply.github.com>
2024-04-11 12:37:02 +0900

    feat: Add order_item entity

--- src/main/java/com/likelion12th/shop/entity/OrderItem.java
+++ src/main/java/com/likelion12th/shop/entity/OrderItem.java
@@ -1,4 +1,34 @@
 package com.likelion12th.shop.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order_item")
+@Getter
 public class OrderItem {
+
+    @Id
+    @Column(name = "order_item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @OneToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private Integer orderPrice;
+
+    private Integer count;
+
+    private LocalDateTime createdBy;
+
+    private LocalDateTime modifiedBy;
+
 }
