846613dacd2e747b2f3b53f60452acedc5dfbcc6
yerim <97941141+yerim123456@users.noreply.github.com>
2024-04-11 12:35:44 +0900

    feat: Add item entity & sell status enum

--- src/main/java/com/likelion12th/shop/entity/Item.java
+++ src/main/java/com/likelion12th/shop/entity/Item.java
@@ -1,4 +1,37 @@
 package com.likelion12th.shop.entity;
 
+import com.likelion12th.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item")
+@Getter
+@Setter
+@ToString
 public class Item {
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String itemName;
+
+    private Integer price;
+
+    private Integer stock;
+
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+
+    private LocalDateTime createdBy;
+
+    private LocalDateTime modifiedBy;
 }
