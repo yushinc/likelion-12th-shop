ef57857ababae9a47cf1ad26d3590fa9eeb3a4b2
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-02 16:56:09 +0900

    feat: Add order & orderItem entity

--- src/main/java/com/likelion12th/shop/entity/Order.java
+++ src/main/java/com/likelion12th/shop/entity/Order.java
@@ -1,16 +1,21 @@
 package com.likelion12th.shop.entity;
 
-
 import com.likelion12th.shop.constant.OrderStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
+@EntityListeners(value = {AuditingEntityListener.class})
 @Getter
-public class Order {
+@Setter
+public class Order extends BaseTime {
 
     @Id
     @Column(name = "order_id")
@@ -21,13 +26,13 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItemList = new ArrayList<>();
+
     private LocalDateTime OrderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
 
-    private LocalDateTime createdBy;
-
-    private LocalDateTime modifiedBy;
-
 }
+
--- src/main/java/com/likelion12th/shop/entity/OrderItem.java
+++ src/main/java/com/likelion12th/shop/entity/OrderItem.java
@@ -2,33 +2,31 @@ package com.likelion12th.shop.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "order_item")
+@EntityListeners(value = {AuditingEntityListener.class})
 @Getter
-public class OrderItem {
+@Setter
+public class OrderItem  extends BaseTime{
 
     @Id
     @Column(name = "order_item_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
     private Integer orderPrice;
 
     private Integer count;
-
-    private LocalDateTime createdBy;
-
-    private LocalDateTime modifiedBy;
 
 }
