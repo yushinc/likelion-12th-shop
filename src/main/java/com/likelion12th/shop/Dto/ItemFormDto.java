f0c64640b5fb9284acc84c78db6a0019b466bb01
jung <dnjswnddkgg@naver.com>
2024-05-03 00:39:07 +0900

    add ItemControlller

--- src/main/java/com/likelion12th/shop/Dto/ItemFormDto.java
+++ src/main/java/com/likelion12th/shop/Dto/ItemFormDto.java
@@ -1,19 +1,46 @@
 package com.likelion12th.shop.Dto;
 
-import com.likelion12th.shop.constant.ItemSellStatus;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import com.likelion12th.shop.entity.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.modelmapper.ModelMapper;
 
+import javax.validation.constraints.NotNull;
 
-@Getter @Setter
+@Getter
+@Setter
 public class ItemFormDto {
+    // 필드
+    private Long id;
+    @NotNull
     private String itemName;
-    private String price;
+    private Integer price;
+    @NotNull
     private Integer stock;
+    @NotNull
+    private String itemSellStatus;
+    @NotNull
     private String itemDetail;
 
-    @Enumerated(EnumType.STRING)
-    private ItemSellStatus itemSellStatus;
+    //get에서만 사용
+    private String itemImgPath;
+
+
+    // modelMapper  객체 생성
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    // Dto -> 엔티티 객체 변환을 위한 메서드
+    public Item createItem(){
+        return modelMapper.map(this, Item.class);
+    }
+
+    // 엔티티 -> Dto 객체 변환을 위한 메서드
+    public static ItemFormDto of(Item item){
+        return modelMapper.map(item, ItemFormDto.class);
+    }
 }
+
+
+
+
+
