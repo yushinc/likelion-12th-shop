8133e2fd39ca6a3977054db8742f67a5410e9665
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-02 16:57:51 +0900

    test: Add order & orderItem test code

---
+++ src/main/java/com/likelion12th/shop/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.likelion12th.shop.repository;
+
+import com.likelion12th.shop.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
