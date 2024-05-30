891cc826f46a38d10a853481c082dbdaf3d1ec7a
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-30 18:14:53 +0900

    feat: Add cartDetail dto

---
+++ src/main/java/com/likelion12th/shop/dto/CartDetailDto.java
@@ -0,0 +1,25 @@
+package com.likelion12th.shop.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CartDetailDto {
+    private Long cartItemId;
+    private String itemName;
+    private int itemPrice;
+    private int count;
+    private String itemImgPath;
+
+    public CartDetailDto(Long cartItemId, String itemName, int itemPrice, int count, String itemImgPath){
+        this.cartItemId = cartItemId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.count = count;
+        this.itemImgPath = itemImgPath;
+
+    }
+}
