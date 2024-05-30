19360605b7b4188e2f155dc4a2d13be626414b6d
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-30 18:20:53 +0900

    feat: Add order dto

--- src/main/java/com/likelion12th/shop/dto/OrderDto.java
+++ src/main/java/com/likelion12th/shop/dto/OrderDto.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,15 +18,17 @@ public class OrderDto {
     private String orderDate;
     private OrderStatus orderStatus;
     private Long itemId;
+    private List<Long> itemIds;
     private int totalPrice;
 
     private static ModelMapper modelMapper = new ModelMapper();
 
     public static OrderDto of(Order order) {
         OrderDto orderDto = modelMapper.map(order, OrderDto.class);
-        if(!order.getOrderItemList().isEmpty()){
-            orderDto.setItemId(order.getOrderItemList().get(0).getItem().getId());
-        }
+
+        orderDto.setItemIds(order.getOrderItemList().stream()
+                .map(orderItem -> orderItem.getItem().getId())
+                .collect(Collectors.toList()));
         return orderDto;
     }
 }
