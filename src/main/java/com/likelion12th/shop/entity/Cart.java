786680ac50b2adb7b9d584be7884825d7e434505
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-30 18:12:40 +0900

    feat: Add cart create business logic

--- src/main/java/com/likelion12th/shop/entity/Cart.java
+++ src/main/java/com/likelion12th/shop/entity/Cart.java
@@ -2,14 +2,14 @@ package com.likelion12th.shop.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "cart")
 @EntityListeners(value = {AuditingEntityListener.class})
 @Getter
+@Setter
 public class Cart extends BaseTime {
 
     @Id
@@ -20,4 +20,11 @@ public class Cart extends BaseTime {
     @OneToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+
+    public static Cart createCart(Member member){
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
 }
