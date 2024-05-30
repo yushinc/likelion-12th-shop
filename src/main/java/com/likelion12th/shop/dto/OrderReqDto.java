c6e9710269d62e0695de475e36155a1abc28ebec
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-30 18:20:43 +0900

    feat: Add orderReq dto

--- src/main/java/com/likelion12th/shop/dto/OrderReqDto.java
+++ src/main/java/com/likelion12th/shop/dto/OrderReqDto.java
@@ -1,11 +1,7 @@
 package com.likelion12th.shop.dto;
 
-import com.likelion12th.shop.constant.OrderStatus;
-import com.likelion12th.shop.entity.Order;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.modelmapper.ModelMapper;
 
 @Getter
 @Setter
