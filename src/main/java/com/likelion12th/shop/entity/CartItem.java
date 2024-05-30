126fc5c417e0255fe3059fb9b41f2dda1d888d46
yerim <97941141+yerim123456@users.noreply.github.com>
2024-05-30 18:13:28 +0900

    feat: Add cartItem create, add/updateCount business logic

--- src/main/java/com/likelion12th/shop/entity/CartItem.java
+++ src/main/java/com/likelion12th/shop/entity/CartItem.java
@@ -2,6 +2,7 @@ package com.likelion12th.shop.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Table(name = "cart_item")
 @EntityListeners(value = {AuditingEntityListener.class})
 @Getter
+@Setter
 public class CartItem extends BaseTime {
 
     @Id
@@ -27,4 +29,19 @@ public class CartItem extends BaseTime {
 
     private Integer count;
 
+    public static CartItem createCartItem(Cart cart, Item item, int count) {
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setItem(item);
+        cartItem.setCount(count);
+        return cartItem;
+    }
+
+    public void addCount(int count){
+        this.count += count;
+    }
+
+    public void updateCount(int count){
+        this.count = count;
+    }
 }
